1dc5fed30c6bc71a351952b8e5a747563990089c
VickY-1997 <vigneshkumar3497@gmail.com>
2024-10-18 02:05:33 +0530

    404 fixed

--- frontEnd/src/home/404.jsx
+++ frontEnd/src/home/404.jsx
@@ -12,7 +12,7 @@ const NotFoundPage = () => {
 					<img src='/netflix-logo.png' alt='Netflix' className='h-8' />
 				</Link>
 			</header>
-			<main className='text-center error-page--content z-10'>
+			<main className='text-center z-10'>
 				<h1 className='text-7xl font-semibold mb-4'>Lost your way?</h1>
 				<p className='mb-6 text-xl'>
 					Sorry, we can't find that page. You'll find lots to explore on the home page.
